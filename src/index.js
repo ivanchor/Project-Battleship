def770f4e6134b21fa5c8f7aa856127ed7c9af13
ivanchor <ichor85@gmail.com>
2026-04-26 16:41:51 -0700

    Changed index.js to run 1 function to start game

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 import "./style.css"
-
-import { ui } from "./ui";
-import { player } from "./player";
-
-const playerA = new player("Human")
-const playerB = new player("CPU")
-playerA.gameboard.addShipHorizontal(0,0,1)
-let boardA = ui.renderBoard(playerA)
-let boardB = ui.renderBoard(playerB)
-const gameScreen = ui.renderGameScreen(playerA, playerB)
+import { runGame } from "./ui-interaction";
 
 
-
-
-const body = document.querySelector("body")
-body.append(gameScreen)
-// body.append(boardA)
-// body.append(boardB)
-// body.append(ui.renderGameScreen())
+runGame()
